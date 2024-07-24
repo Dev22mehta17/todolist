487db00ad063353db8908c8847c895baa10d72d3
dev mehta <mehtadev2004@gmail.com>
2024-07-25 01:18:22 +0530

    some

--- app/page.js
+++ app/page.js
@@ -4,11 +4,21 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log(title)
+    console.log(desc)
+    settitle("")
+    setdesc("")
+    
+  }
   return (
     <>
     <h1 className='bg-black text-white p-5 text-xl font-bold text-center'>Dev's TodoList</h1>
     
-    <form>
+    <form onSubmit={submitHandler}>
+
       <input type="text" className='text-xl border-zinc-800 border-4 m-5 px-4 py-2' placeholder='Enter Title Here' value={title}
       onChange={(e)=>{
         settitle(e.target.value)
